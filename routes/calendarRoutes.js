adbc50d60ca5b7528698548063052e81e19545c4
Ulises Ros <ulisesros70@gmail.com>
2025-06-21 22:05:58 -0300

    cambios

--- routes/calendarRoutes.js
+++ routes/calendarRoutes.js
@@ -11,6 +11,6 @@ router.post('/reset-temporales', auth, resetUserSelections)
 router.post('/cancelar-temporalmente', auth, eliminarTurnoPorEstaSemana);
 router.post('/feriado', auth, marcarFeriado);
 router.get('/feriados', auth, getFeriados);
-router.delete('/eliminar-feriado', auth, quitarFeriado);
+router.post('/quitar-feriado', auth, quitarFeriado);
 
 module.exports = router;
