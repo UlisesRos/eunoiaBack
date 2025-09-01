1f2e692af14e96212a1a9261ab941c0242580b18
Ulises Ros <ulisesros70@gmail.com>
2025-09-01 09:18:17 -0300

    Up resetCambioMensual.js

--- cronJobs/resetCambioMensual.js
+++ cronJobs/resetCambioMensual.js
@@ -35,7 +35,7 @@ const resetCambioMensual = async () => {
 const resetCambiosSemanales = () => {
     // ✅ Reiniciar cambios semanales
     // Cada sábado a las 15:00
-    cron.schedule('* * * * *', async ()=> {
+    cron.schedule('0 7 * * 6', async ()=> {
     console.log('[CRON] Reiniciando cambios semanales (temporalSelections)');
 
     try {
